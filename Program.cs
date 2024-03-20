2c440f54aa9217b88a5cb23eef0faa9999acb784
DaviEduard0x01 <davibarbosafr@gmail.com>
2024-03-20 16:21:15 -0300

    cmmt05

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um Número ");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                Console.WriteLine(num + "  é divisível por 3 e por 5");
+            }
+            else
+            {
+                Console.WriteLine("Número Inválido ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
